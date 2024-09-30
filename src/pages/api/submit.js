b719ffdcdbcfe52f53372d9c13631fcc56dd9185
Lili Fuentes <lile7886@gmail.com>
2024-09-30 19:52:53 -0400

    moved cors to inside the handler

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -2,20 +2,18 @@ import { getSecrets } from '../../utils/aws_secrets';
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 
-function enableCors(req, res) {
+export default async function handler(req, res) {
+  
   res.setHeader('Access-Control-Allow-Origin', 'https://www.cfuentherapy.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
-    return true;
+    return;
   }
-  return false;
-
-}
-
-export default async function handler(req, res) {
+  
+  
   if (req.method !== 'POST') {
     // Respond with 405 Method Not Allowed for any non-POST requests
     return res.status(405).json({ error: 'POST method not allowed.' });
