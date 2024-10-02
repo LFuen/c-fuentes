71d82905197dee163c7a0c259ab5be3a74a49484
Lili Fuentes <lile7886@gmail.com>
2024-10-02 12:52:45 -0400

    added utils and secrets to amplify

---
+++ amplify/backend/function/cfunTherapyLambda/src/utils/aws_secrets.js
@@ -0,0 +1,29 @@
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+  } from "@aws-sdk/client-secrets-manager";
+  
+  const secret_name = "cfuen-email";
+  
+  export const getSecrets = async () => {
+    const client = new SecretsManagerClient({
+      region: "us-east-1",
+    });
+  
+    try {
+      const response = await client.send(
+        new GetSecretValueCommand({
+          SecretId: secret_name,
+          VersionStage: "AWSCURRENT", 
+        })
+      );
+  
+      const secrets = JSON.parse(response.SecretString);
+      return secrets;
+    } catch (error) {
+      console.log('Error retrieving secret:', error);
+      console.error('Error retrieving secret:', error);
+      throw error;
+    }
+  };
+  
